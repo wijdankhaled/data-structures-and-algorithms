63557ace9a982726b3e50d5dcce76885bfa128da
wijdankhaled <wijdankhaled178@gmail.com>
2021-08-16 22:09:17 +0300

    chang

---
+++ 401challennging/array-reverse/array-reverse.js
@@ -0,0 +1,11 @@
+'use strict '
+reversFun =()=>{
+    let arr=[1,2,3];
+    let reversarr=[];
+    for(let i=arr.length-1;i>=0;i--){
+      reversarr.push(arr[i])
+    }
+    return reversarr;
+    }
+    
+    reversFun();
