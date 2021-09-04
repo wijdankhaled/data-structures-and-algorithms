0ae78b136b97dfa8803d59a8705579fe79b19535
wijdankhaled <wijdankhaled178@gmail.com>
2021-09-05 00:54:56 +0300

    code 15

---
+++ 401challennging/trees/__test__/node.test.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const Node = require("../node");
+
+describe("Node Module", () => {
+  it("constructor", () => {
+    let value = 5;
+    let newNode = new Node(value);
+    expect(newNode.value).toEqual(value);
+    expect(newNode.left).toBeNull();
+    expect(newNode.right).toBeNull();
+  });
+  
+});
